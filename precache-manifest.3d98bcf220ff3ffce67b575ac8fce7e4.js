323772b1f178014f53c2952fd15773f451a84267
Pratik Wadekar <pratik.wadekar2810@gmail.com>
2019-10-31 20:37:31 +0530

    Updates

---
+++ precache-manifest.3d98bcf220ff3ffce67b575ac8fce7e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa867bbb865436b17e9e021275aec0af",
+    "url": "/react-video-search/index.html"
+  },
+  {
+    "revision": "7edd5223514a7b12cf3c",
+    "url": "/react-video-search/static/css/main.d8cd2c6d.chunk.css"
+  },
+  {
+    "revision": "6f90c43f91de0464aa47",
+    "url": "/react-video-search/static/js/2.62f10598.chunk.js"
+  },
+  {
+    "revision": "7edd5223514a7b12cf3c",
+    "url": "/react-video-search/static/js/main.4a517003.chunk.js"
+  },
+  {
+    "revision": "0855f33685d2f580754f",
+    "url": "/react-video-search/static/js/runtime-main.761b5066.js"
+  }
+]);
